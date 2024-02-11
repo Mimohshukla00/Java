84661919a197e2d2ea23a8617da46dca93eb02cd
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-02-11 15:46:38 +0530

    config all files such as functions and pattern printing

---
+++ functions2.java
@@ -0,0 +1,8 @@
+public class functions2 {
+    public static void main(String[] args){
+
+    }
+
+      
+    
+}
---
+++ pattern3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args){
+       
+        int num=5;
+        for (int i = 1; i <=num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
