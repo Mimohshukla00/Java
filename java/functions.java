1ca876ebdb2cdc0fd24be345af15cf4e0073c15e
mimoh shukla <mimohshukla0001@gmail.com>
2024-02-11 17:01:20 +0530

    Add files via upload
    config all files 
    
    Signed-off-by: mimoh shukla <mimohshukla0001@gmail.com>

---
+++ java/functions.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class functions {
+    public static void main(String[] args){
+        sum();
+        // sum();
+        // sum();
+
+
+    }
+     static void sum(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a first number:");
+        int num1 = input.nextInt();
+        System.out.println("Enter a second number:");
+        int num2= input.nextInt();
+        int sum=num1+num2;
+        System.out.println("The sum of num1 and num2 is="+ sum);
+
+    }
+
+    /*
+     access modifier ( we will look in oop)
+     */
+    
+}
---
+++ java/pattern1.java
@@ -0,0 +1,18 @@
+public class pattern1 {
+    public static void main(String[] args ){
+
+        int n =4;
+        int m=5;
+        //outer loop
+        for (int i = 1; i <= n; i++) {
+            //innerloop
+            for (int j = 1; j <=m; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            
+        }
+           }
+    
+}
